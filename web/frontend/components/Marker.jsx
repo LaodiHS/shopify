478e1efc19504f1f4f9a8269e6da31c9151ac36a
wujimu <hasanseirafi69@gmail.com>
2023-11-01 11:30:10 -0700

    remove styled component

--- web/frontend/components/Marker.jsx
+++ web/frontend/components/Marker.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 import {
-  IonText,
   IonThumbnail,
-  IonGrid,
   IonCol,
   IonRow,
   IonItem,
-  IonImg,
   IonChip,
   IonCard,
-  IonCardContent,
   IonCardTitle,
   IonCardHeader,
-  IonSkeletonText,
 } from "@ionic/react";
 // import { useDataProvidersContext, NoImagePlaceHolder } from "../components";
 // import { darkenHexColor } from "../utilities/darkenHexColor";
@@ -25,16 +20,14 @@ const Marker = ({ markerType, label, color, size, loadingStream, imageSrc }) => 
   ) {
     throw Error("Invalid argument types");
   }
-
-  
-    return markerType ==="img" ? <PlaceImage color={color} imageSrc = {imageSrc} size={size} loadingStream ={loadingStream} /> : <PlaceIonChip label={label} color={color} /> ;
+  return markerType ==="img" ? <PlaceImage color={color} imageSrc = {imageSrc} size={size} loadingStream ={loadingStream} /> : <PlaceIonChip label={label} color={color} /> ;
 };
 
 function PlaceImage({ color, imageSrc, size, loadingStream }) {
   return (
-    <IonRow key={color + "Row"}>
-      <IonCol key={color + "col"} size="12" className="ion-align-self-center">
-        <IonItem key={color + "IonItem"} lines="none">
+    <IonRow key={`${color}Row`}>
+      <IonCol key={`${color}col`} size="12" className="ion-align-self-center">
+        <IonItem key={`${color}IonItem`} lines="none">
          
          {loadingStream ? (<IonCard animated={true} style={{
           // width:"140px", height:"140px",
@@ -42,17 +35,17 @@ function PlaceImage({ color, imageSrc, size, loadingStream }) {
           style={{fontFamily:"Baloo, sans-serif"}}
           >Loading Image...</IonCardTitle></IonCardHeader></IonCard>) : (
           <IonThumbnail
-            key={"thumb" + color}
+            key={`thumb${color}`}
             aria-hidden="true"
             slot="start"
             style={{
-              "--size": size - 20 + "px",
+              "--size": `${size - 20}px`,
               "--borderRadius": "14px",
               "border": '6px solid' +  color
             }}
           >
             <img
-              key={color + "IonImg"}
+              key={`${color}IonImg`}
               alt="Image Requirement"
               src={imageSrc}
               loading="eager"
@@ -70,7 +63,7 @@ function PlaceImage({ color, imageSrc, size, loadingStream }) {
 function PlaceIonChip({ label, color }) {
   return (
     <IonChip
-      key={"sub" + color}
+      key={`sub${color}`}
       className="ion-text-capitalize"
       style={{
         // verticalAlign: "super",
